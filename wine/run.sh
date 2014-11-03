14eeb012ea134b11c6f8ebebc8f24330e3502ee1
George Schneeloch <bostonbusmap@gmail.com>
2014-11-02 19:18:07 -0500

    Dockerfile for running Steam within wine

---
+++ wine/run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+COMMAND=/bin/bash
+
+xhost + # allow connections to X server
+docker run --privileged -e "DISPLAY=unix:0.0" -v="/tmp/.X11-unix:/tmp/.X11-unix:rw"  -d -p 127.0.0.1:2222:22 wine
